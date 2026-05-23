fd93cec166b4830374f3103b6a967260cca5e1eb
Nathaniel Nicholas <git@nathanielnicholas.com>
2026-05-23 16:32:23 +0200

    Minimal implementation of Name

---
+++ app/fitstrive/lib/domain/entities/user.dart
@@ -0,0 +1,8 @@
+import '../value_objects/name.dart';
+
+/// Represents a User.
+final class User {
+  final Name name;
+
+  User({required this.name});
+}
---
+++ app/fitstrive/lib/domain/value_objects/name.dart
@@ -0,0 +1,7 @@
+/// Represents a name.
+final class Name {
+  final String firstName;
+  final String? lastName;
+
+  Name({required this.firstName, this.lastName});
+}
