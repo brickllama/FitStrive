1fc5511b6fdf27cc3b52eaa8b49ed1cb86110bea
Nathaniel Nicholas <git@nathanielnicholas.com>
2026-05-24 18:15:56 +0200

    Probably gonna break the server

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
 });
 
 export default pool;
