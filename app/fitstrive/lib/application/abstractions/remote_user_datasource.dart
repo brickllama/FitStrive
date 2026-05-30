666ed0bdc42b230255a3b115ea52401b3352ba7b
Nathaniel Nicholas <git@nathanielnicholas.com>
2026-05-30 11:12:50 +0200

    CRUD updates

--- app/fitstrive/lib/application/abstractions/remote_user_datasource.dart
+++ app/fitstrive/lib/application/abstractions/remote_user_datasource.dart
@@ -3,8 +3,13 @@ import 'package:fitstrive/domain/entities/user.dart';
 import 'package:fitstrive/domain/value_objects/email.dart';
 import 'package:fitstrive/domain/value_objects/name.dart';
 import 'package:fitstrive/domain/value_objects/password.dart';
+import 'package:fitstrive/domain/value_objects/user_id.dart';
 
 abstract interface class RemoteUserDatasource {
+  Future<Result<void>> delete(final UserID userID);
+
+  Future<Result<User>> login(final Email email, final Password password);
+
   Future<Result<User>> register(
     final Email email,
     final Name name,
