d238d5bf465f0078d8f4781c7ba43d9c13d4896a
helenamajer <helena.majer@yahoo.com>
2026-05-30 12:17:15 +0200

    daily intake stats implemented

--- app/fitstrive/lib/main.dart
+++ app/fitstrive/lib/main.dart
@@ -3,20 +3,22 @@ import 'package:fitstrive/application/usecases/register_usecase.dart';
 import 'package:fitstrive/data/datasources/remote_user_datasource_impl.dart';
 import 'package:fitstrive/data/datasources/user_local_source.dart';
 import 'package:fitstrive/data/repositories/user_repository_impl.dart';
+import 'package:fitstrive/features/food_tracking/data/datasources/food_local_data_source.dart';
 import 'package:fitstrive/presentation/viewmodels/auth/login_viewmodel.dart';
 import 'package:fitstrive/presentation/viewmodels/auth/register_viewmodel.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'core/database/fitstrive_database.dart';
 import 'presentation/presentation.dart';
 
 void main() async {
   await dotenv.load(fileName: 'config/.env');
-  AppDatabase db = AppDatabase();
-  final remoteUserDataSource = RemoteUserDatasourceImpl();
-  final userLocalSource = UserLocalSourceImpl(db);
 
+  final db = AppDatabase();
+  final foodLocalDataSource = FoodLocalDataSourceImpl(db);
+  final userLocalSource = UserLocalSourceImpl(db);
+  final remoteUserDataSource = RemoteUserDatasourceImpl();
   final userRepository = UserRepositoryImpl(
     localSource: userLocalSource,
     remoteSource: remoteUserDataSource,
@@ -24,19 +26,23 @@ void main() async {
   final loginUseCase = LoginUseCase(userRepository);
   final registerUseCase = RegisterUseCase(userRepository);
 
-  runApp(
-    FitStriveApp(loginUseCase: loginUseCase, registerUseCase: registerUseCase),
-  );
+  runApp(FitStriveApp(
+    loginUseCase: loginUseCase,
+    registerUseCase: registerUseCase,
+    foodLocalDataSource: foodLocalDataSource,
+  ));
 }
 
 class FitStriveApp extends StatelessWidget {
   final LoginUseCase loginUseCase;
   final RegisterUseCase registerUseCase;
+  final FoodLocalDataSourceImpl foodLocalDataSource;
 
   const FitStriveApp({
     super.key,
     required this.loginUseCase,
     required this.registerUseCase,
+    required this.foodLocalDataSource,
   });
 
   @override
@@ -64,8 +70,7 @@ class FitStriveApp extends StatelessWidget {
           '/login': (_) => const LoginView(),
           '/register': (_) => const RegisterView(),
           '/forgot-password': (_) => const ForgotPasswordView(),
-          '/daily-intake-stats': (_) => const DailyIntakeView(),
-      },
+        },
       ),
     );
   }
